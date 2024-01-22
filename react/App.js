764c55801d845e48bbe593e5af278ef6538b354f
DimaAmelchenkoG <144106912+DimaAmelchenkoG@users.noreply.github.com>
2024-01-22 11:50:56 +0300

    Add files via upload

--- react/App.js
+++ react/App.js
@@ -3,12 +3,17 @@ import Header from './components/Header';
 import Block2 from './components/Block2';
 import { useState } from 'react';
 import RegistrationPage from './components/RegistrationPage';
-
+import {useMatchMedia} from './hooks/use-match-media';
 
 function App() {
   const [page, setPage] = useState('reg');
+  
+  const { isMobile, isTablet, isDesktop } = useMatchMedia();
   return (
     <div className="App">
+      {isMobile && <h2>This is mobile</h2>}
+      {isTablet && <h2>This is tablet</h2>}
+      {isDesktop && <h2>This is desktop</h2>}
       <Header/>
       {page === 'main' && (
         <>
